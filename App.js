6568009d4c3bf0423e2182255f8659784738da21
WatchJani <jankokondic84@gmail.com>
2024-05-20 01:02:12 +0200

    init map

--- App.js
+++ App.js
@@ -1,20 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import Mapbox from '@rnmapbox/maps';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+Mapbox.setAccessToken('pk.eyJ1IjoiamFua283MjYyIiwiYSI6ImNsdzEzMTg1YTA2cnkyanBoeW9xaTRpZ3QifQ.7vbQ1Xgo0cBUlpxFx8LveQ');
+const App = () => {
+    return (
+        <View style={styles.page}>
+            <Mapbox.MapView style={styles.map} styleURL='mapbox://styles/mapbox/dark-v11' />
+        </View>
+    );
 }
 
+export default App;
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    page: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    container: {
+        height: 300,
+        width: 300,
+    },
+    map: {
+        flex: 1
+    }
 });
